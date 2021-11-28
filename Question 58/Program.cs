ff15f985f8f56b2e9cfec0d80651aa288b7f1cc0
radia sabahat <radiasabahat@gmail.com>
2021-11-28 12:27:17 -0800

    first commit

---
+++ Question 58/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 2, b = 3, c = 5;
+
+//(a)
+Console.WriteLine("a");
+int a1=++a;
+Console.WriteLine("Valid Statement");
+Console.WriteLine(a1);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//(b)
+Console.WriteLine("b");
+Console.WriteLine("Invalid Statement");
+
+//(c)
+Console.WriteLine("c");
+int a2=a++;
+Console.WriteLine("Valid Statement");
+Console.WriteLine(a2);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//(d)
+Console.WriteLine("d");
+b += a;
+Console.WriteLine("Valid Statement");
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//(e)
+Console.WriteLine("e");
+b += a * c;
+Console.WriteLine("Valid Statement");
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//(f)
+Console.WriteLine("f");
+Console.WriteLine("Invalid Statement");
+
+//(g)
+Console.WriteLine("g");
+a += 2;
+b = 2 / 2;
+c = a * b;
+Console.WriteLine("Valid Statement");
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+//(h)
+
+Console.WriteLine("h");
+Console.WriteLine("Invalid Statement");
+
+//(i)
+Console.WriteLine("i");
+int b3 = a++ + b++;
+Console.WriteLine("Valid Statement");
+Console.WriteLine(a);
+Console.WriteLine(b3);
+Console.WriteLine(c);
